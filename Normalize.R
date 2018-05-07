0ccf6fa7c5484a6c1b0912d14d44b478b90dce37
Shane P <shane.pullens@hotmail.com>
2018-05-07 15:37:48 +0200

    Pathway weggehaald

--- Normalize.R
+++ Normalize.R
@@ -1,7 +1,7 @@
 #Normalising genes
-Counts <- read.delim('/home/shane/Documents/Course_11/data/RNA-Seq-counts.txt', header = TRUE, skip =1, row.names = 1)
+Counts <- read.delim('***PATH***/RNA-Seq-counts.txt', header = TRUE, skip =1, row.names = 1)
 
 CountsNorm <- Counts / colSums(Counts)[col(Counts)]
 
 library("xlsx")
-write.xlsx(CountsNorm, "/home/shane/Documents/Course_11/data/countsNorm.xlsx")
+write.xlsx(CountsNorm, "***PATH***/countsNorm.xlsx")
