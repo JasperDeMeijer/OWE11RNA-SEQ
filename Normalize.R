cb838cc764370c583ba647df0f10b6506363644e
Shane P <shane.pullens@hotmail.com>
2018-06-15 23:41:19 +0200

    Final clean

--- Normalize.R
+++
@@ -1,7 +0,0 @@
-#Normalising genes
-Counts <- read.delim('***PATH***/RNA-Seq-counts.txt', header = TRUE, skip =1, row.names = 1)
-
-CountsNorm <- Counts / colSums(Counts)[col(Counts)]
-
-library("xlsx")
-write.xlsx(CountsNorm, "***PATH***/countsNorm.xlsx")
